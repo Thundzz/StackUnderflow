41cb3941bacf8bc2d82a2212456d938851d62872
Thundzz <yacinenew@gmail.com>
2014-01-06 23:08:27 +0100

    voila

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,6 +2,7 @@ StackUnderflow::Application.routes.draw do
 
 
   resources :posts
+  root :to => 'posts#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
