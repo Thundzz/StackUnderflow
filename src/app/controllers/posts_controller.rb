4d6725877e0d3e1e98a241b9911012d23698877e
Thundzz <yacinenew@gmail.com>
2014-01-08 00:34:32 +0100

    flash messages ok

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -6,13 +6,17 @@ class PostsController < ApplicationController
 
 	end
 
+	def show
+		
+	end
+
+	def method_name
+		
+	end
+
 	def edit
 
 		@post = Post.find( params[:id] )
-		@post.editionNo = @post.editionNo + 1
-		postnumber = @post.editionNo
-		@post.save()
-		redirect_to posts_path 
 
 	end
 
@@ -22,12 +26,31 @@ class PostsController < ApplicationController
 
 	end
 
+	def update
+		@newpost = params[:post]
+		@oldpost = Post.find( params[:id] )
+		@oldpost.content = @newpost[:content]
+		@oldpost.title = @newpost[:title]
+		@oldpost.editionNo = @oldpost.editionNo + 1
+		@oldpost.save()
+		redirect_to posts_path 
+		
+	end
+
 	def create
 		@post = Post.new(params[:post])
 		@post.editionNo = 0
-		@post.save
+		if @post.save
+			redirect_to posts_path, :notice => "Votre post a bien été ajouté"
+		else
+			render "new"
+		end
+	end
+
+	def destroy
+		@post = Post.find( params[:id])
+		@post.destroy
 		redirect_to posts_path 
-		
 	end
 
 end
