363c8a43225d90f85d408e1d913149f13b0cb1c4
sjemli <seifeddinejemli@gmail.com>
2014-02-05 22:24:07 +0100

    Tags resolution conflit avec version Elorri

--- src/app/controllers/tags_controller.rb
+++ src/app/controllers/tags_controller.rb
@@ -16,13 +16,10 @@ end
   # GET /tags/1
   # GET /tags/1.json
   def show
-    @tag = Tag.find(params[:id])
+    @tag= Tag.find(params[:id])
     @questions = @tag.questions
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @tag }
-    end
   end
+
 
   # GET /tags/new
   # GET /tags/new.json
@@ -42,17 +39,14 @@ end
 
   # POST /tags
   # POST /tags.json
-  def create
-    @tag = Tag.new(params[:tag])
 
-    respond_to do |format|
-      if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render json: @tag, status: :created, location: @tag }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
+  def create
+    puts(params[:tag])
+    @tag = Tag.new(params[:tag])
+    if @tag.save
+      redirect_to tags_path, :notice => "Votre tag a bien ete ajoute"
+    else
+      render "new"
     end
   end
 
