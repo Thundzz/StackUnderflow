5955960a9aa241c3a89876afac0771e102cd9c51
Florent Bouquet <florent.b.bouquet@orange.fr>
2014-02-07 15:02:04 +0100

    Suppression des migrate remove

--- db/migrate/20140204213243_remove_user_from_posts.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserFromPosts < ActiveRecord::Migration
-  def up
-    remove_column :posts, :user_id
-  end
-
-  def down
-    add_column :posts, :user_id, :integer
-  end
-end
--- db/migrate/20140204213255_remove_user_from_questions.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserFromQuestions < ActiveRecord::Migration
-  def up
-    remove_column :questions, :user_id
-  end
-
-  def down
-    add_column :questions, :user_id, :integer
-  end
-end
