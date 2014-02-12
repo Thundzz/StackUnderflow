047de5ef26270634b97c1441b5931cffe416b7ec
sjemli <seifeddinejemli@gmail.com>
2014-02-12 05:17:09 +0100

    intégration de l editeur de texte

---
+++ app/assets/javascripts/ckeditor/build-config.js
@@ -0,0 +1,59 @@
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/3fd8e7cea4bb942a140e2fa5931069da
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/3fd8e7cea4bb942a140e2fa5931069da
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono',
+	preset: 'basic',
+	ignore: [
+		'dev',
+		'.gitignore',
+		'.gitattributes',
+		'README.md',
+		'.mailmap'
+	],
+	plugins : {
+		'about' : 1,
+		'basicstyles' : 1,
+		'clipboard' : 1,
+		'codemirror' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'eqneditor' : 1,
+		'floatingspace' : 1,
+		'indent' : 1,
+		'link' : 1,
+		'list' : 1,
+		'print' : 1,
+		'syntaxhighlight' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1,
+		'fr' : 1
+	}
+};
