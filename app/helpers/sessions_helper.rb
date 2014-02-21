dd49b96a85734758d12214f1f8e505852bbf4d68
Florent Bouquet <florent.b.bouquet@orange.fr>
2014-02-21 14:58:32 +0100

    Modification pour le login / la création de User par CAS

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,6 +31,27 @@ module SessionsHelper
     session[:return_to] = request.url if request.get?
   end
 
+
+  def create_or_login_user(login)
+    # On teste d'abord la présence ou non du User
+    user = User.find_by_login(login)
+    if user.nil?
+      user = User.new(login: login)
+      if user.save
+        sign_in user
+          flash[:success] = "Connexion par le CAS réussie. Nous vous encourageons à renseigner votre profil pour optimiser votre utilisation de StackUnderflow !"
+          redirect_back_or user
+      else
+        flash[:error] = "Connexion avortée : veuillez réessayer."
+        redirect_to new_session_path
+      end
+    else
+      sign_in user
+      redirect_back_or user
+    end
+  end
+  
+
   private
 
     def user_from_remember_token
