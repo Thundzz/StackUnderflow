afa0cdd1384385b8263d27fc180595fbbebef032
Florent Bouquet <florent.b.bouquet@orange.fr>
2014-03-14 15:01:16 +0100

    Récupération du bon tri sur les réponses

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 # coding: utf-8 
 
 class Answer < ActiveRecord::Base
-  scope :stable_sort, order("validated, score, created_at DESC")
+  scope :stable_sort, order("validated DESC, score DESC, created_at DESC")
   
   acts_as_voteable
   attr_accessible :content, :score, :validated, :question_id, :user, :question
