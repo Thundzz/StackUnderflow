b944b18ff2135440e8d85cab491a8fed78f33379
esagardia <esagardia@esagardia-HP-Pavilion-dv6-Notebook-PC.(none)>
2014-03-14 15:41:22 +0100

    suppression avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,3 @@
 
 module ApplicationHelper
-  def avatar_url(user,taille)
-    default_url = "#{root_url}images/guest.png"
-    if user.email.nil?
-    
-    "http://gravatar.com/avatar/?s="+taille.to_s+"&d=mm"
-    else
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s="+taille.to_s+"&d=wavatar"
-    end
- end
 end
